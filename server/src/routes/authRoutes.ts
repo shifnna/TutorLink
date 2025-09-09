6348f6e3f93bfc56ea15a0afea95774d01818546
shifna majeed <sshifnamajeed@gmail.com>
2025-09-09 18:17:25 +0530

    forgot-pass & resend-OTP

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -6,6 +6,7 @@ const router = Router();
 router.post('/signup',AuthController.signup)
 router.post("/verify-otp", AuthController.verifyOtp);
 router.post("/resend-otp", AuthController.resendOtp);
+router.post("/reset-password", AuthController.resetPassword);
 router.post('/login',AuthController.login)
 
 export default router;
